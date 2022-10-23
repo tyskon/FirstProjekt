88c366b9988c5a25d8ce378d58bbabe91410c6a4
Alexander <aliaksandrkisialeuski@gmail.com>
2022-10-23 19:21:28 +0200

    Sysout is .....

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("My second life");
+        System.out.println("My second life is...");
     }
 }
